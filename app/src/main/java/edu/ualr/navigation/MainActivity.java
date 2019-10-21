2828692cb190cf006cd84d80bc25b19e6dcc8563
irconde <ivanrodriguezconde@gmail.com>
2019-10-21 15:43:21 -0500

    starter code

---
+++ app/src/main/java/edu/ualr/navigation/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.ualr.navigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+// TODO 01. We have to create the Fragment classes that we are going to use to instantiate from the activity
+// FormFragment and PizzaOrderFragment
+// TODO 02. Define the corresponding layout files
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.pager);
+        pageAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pageAdapter);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+        private static final int NUM_PAGES = 2;
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        // TODO 03. We instantiate the proper Fragment class based on the current position of the View pager
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
