b83bfe82470a5786b50212a27c13629155ac416b
irconde <ivanrodriguezconde@gmail.com>
2019-10-22 15:25:58 -0500

    View pager with several views

--- app/src/main/java/edu/ualr/navigation/MainActivity.java
+++ app/src/main/java/edu/ualr/navigation/MainActivity.java
@@ -1,61 +1,44 @@
 package edu.ualr.navigation;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-// TODO 01. We have to create the Fragment classes that we are going to use to instantiate from the activity
-// FormFragment and PizzaOrderFragment
-// TODO 02. Define the corresponding layout files
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int NUM_PAGES = 2;
-
-    private static final int PIZZA_FRAGMENT_IDX = 0;
-    private static final int CONTACT_FRAGMENT_IDX = 1;
-
-    private ViewPager2 viewPager;
-    private FragmentStateAdapter pageAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.pager);
-        pageAdapter = new ScreenSlidePagerAdapter(this);
+        ViewPager2 viewPager = findViewById(R.id.pager);
+        // TODO 01. We create an Adapter that extends from RecyclerView.Adapter
+        SimpleAdapter pageAdapter = new SimpleAdapter(generateDummyDataset());
         viewPager.setAdapter(pageAdapter);
     }
 
-    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
-
-        public ScreenSlidePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-        }
-
-        // TODO 03. We instantiate the proper Fragment class based on the current position of the View pager
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            switch (position) {
-                case PIZZA_FRAGMENT_IDX:
-                    return new PizzaOrderFragment();
-                case CONTACT_FRAGMENT_IDX:
-                    return new FormFragment();
-                    default:
-                        return null;
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            return NUM_PAGES;
-        }
+    private List<Contact> generateDummyDataset() {
+        List<Contact> contacts = new ArrayList<>();
+        Contact firstContact = new Contact();
+        firstContact.setFirstName("Ivan");
+        firstContact.setLastName("Rodriguez-Conde");
+        firstContact.setAddress("C/ Xoan Manuel Pintos 6, 6D Ourense");
+        firstContact.setPhoneNumber("+34 647858682");
+        firstContact.setWebsite("http://www.sketcharm.com");
+        firstContact.setEmailAddress("irconde@ualr.edu");
+        contacts.add(firstContact);
+        Contact secondContact = new Contact();
+        secondContact.setFirstName("Elvira");
+        secondContact.setLastName("Losada-Sanchez");
+        secondContact.setAddress("C/ Xoan Manuel Pintos 6, 6D Ourense");
+        secondContact.setPhoneNumber("+34 865786345");
+        secondContact.setWebsite("http://www.elviralosada.com");
+        secondContact.setEmailAddress("elvira.losada@gmail.com");
+        contacts.add(secondContact);
+        return contacts;
     }
+
 }
---
+++ app/src/main/java/edu/ualr/navigation/SimpleAdapter.java
@@ -0,0 +1,61 @@
+package edu.ualr.navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Created by irconde on 2019-10-22.
+ */
+// TODO 01. We create an Adapter that extends from RecyclerView.Adapter
+public class SimpleAdapter extends RecyclerView.Adapter {
+
+    List<Contact> mContacts;
+
+    public SimpleAdapter(List<Contact> contacts) {
+        this.mContacts = contacts;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater =  LayoutInflater.from(parent.getContext());
+        return new ContactViewHolder(inflater.inflate(R.layout.contact, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ContactViewHolder vh = (ContactViewHolder) holder;
+        Contact contact = this.mContacts.get(position);
+        vh.name.setText(contact.getFullName());
+        vh.website.setText(contact.getWebsite());
+        vh.phoneNumber.setText(contact.getPhoneNumber());
+        vh.address.setText(contact.getAddress());
+        vh.email.setText(contact.getEmailAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mContacts.size();
+    }
+
+    private class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView name, phoneNumber, email, address, website;
+
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.fullNameTV);
+            phoneNumber = itemView.findViewById(R.id.phoneNumberTV);
+            email = itemView.findViewById(R.id.emailTV);
+            address = itemView.findViewById(R.id.addressTV);
+            website = itemView.findViewById(R.id.webTV);
+        }
+    }
+}
