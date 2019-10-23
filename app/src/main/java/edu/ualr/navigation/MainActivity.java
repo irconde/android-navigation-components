554a072a3ffa156539b41fc0eaa4cef53632cb07
irconde <ivanrodriguezconde@gmail.com>
2019-10-22 23:40:33 -0500

    TabLayout properties implemented

--- app/src/main/java/edu/ualr/navigation/MainActivity.java
+++ app/src/main/java/edu/ualr/navigation/MainActivity.java
@@ -7,17 +7,23 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class MainActivity extends AppCompatActivity {
+// TODO 05. Add more tabs to the activity so we can see the behavior of the scrollable TabLayout
 
-    private static final int NUM_PAGES = 2;
+public class MainActivity extends AppCompatActivity implements PizzaOrderFragment.OnPlaceOrderListener {
+
+    private static final int NUM_PAGES = 4;
 
     private static final int PIZZA_FRAGMENT_IDX = 0;
     private static final int CONTACT_FRAGMENT_IDX = 1;
+    private static final int PIZZA_FRAGMENT_COPY_IDX = 2;
+    private static final int CONTACT_FRAGMENT_COPY_IDX = 3;
 
     private ViewPager2 viewPager;
     private FragmentStateAdapter pageAdapter;
@@ -31,20 +37,43 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabs);
         pageAdapter = new ScreenSlidePagerAdapter(this);
         viewPager.setAdapter(pageAdapter);
-        // TODO 02. We create a new TabLayoutMediator to bind the viewPager to the tab layout
         new TabLayoutMediator(tabLayout, viewPager,
                 new TabLayoutMediator.TabConfigurationStrategy() {
                     @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                        // TODO 03. Set tab labels
                        onTabNameSet(tab, position);
+                       onTabIconSet(tab, position);
                     }
                 }).attach();
     }
 
-    // TODO 03. Set tab labels
+    // TODO 07. We create a new method that adds an icon to each tab -->
+    private void onTabIconSet (TabLayout.Tab tab, int position) {
+        TypedArray icons = getResources().obtainTypedArray(R.array.icons);
+        int iconId = icons.getResourceId(position, -1);
+        if (iconId != -1) {
+            tab.setIcon(iconId);
+        }
+    }
+
+    // TODO 08. Create and add a badge to a tab
+    private void incrementBadgeCounter(TabLayout.Tab tab) {
+        // TODO 09. Create and initialize an instance of BadgeDrawable.
+        // Subsequent calls to this method will reuse the existing BadgeDrawable
+        BadgeDrawable badge = tab.getOrCreateBadge();
+        badge.setVisible(true);
+        // TODO 10. Show a number in the badge
+        badge.setNumber(badge.getNumber() + 1);
+    }
+
     private void onTabNameSet(TabLayout.Tab tab, int position) {
         String[] tabLabels = getResources().getStringArray(R.array.tabs);
         tab.setText(tabLabels[position]);
+    }
+
+    // TODO 14. We implement the interface methods
+    @Override
+    public void onPlaceOrderButtonClicked() {
+        incrementBadgeCounter(tabLayout.getTabAt(0));
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
@@ -60,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
                 case PIZZA_FRAGMENT_IDX:
                     return new PizzaOrderFragment();
                 case CONTACT_FRAGMENT_IDX:
+                    return new FormFragment();
+                case PIZZA_FRAGMENT_COPY_IDX:
+                    return new PizzaOrderFragment();
+                case CONTACT_FRAGMENT_COPY_IDX:
                     return new FormFragment();
                     default:
                         return null;
--- app/src/main/java/edu/ualr/navigation/PizzaOrderFragment.java
+++ app/src/main/java/edu/ualr/navigation/PizzaOrderFragment.java
@@ -1,5 +1,6 @@
 package edu.ualr.navigation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +10,42 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 /**
  * Created by irconde on 2019-10-22.
  */
 public class PizzaOrderFragment extends Fragment {
+
+    // TODO 11. Define an interface to communicate the fragment with the activity
+
+    public interface OnPlaceOrderListener {
+        void onPlaceOrderButtonClicked();
+    }
+
+    private OnPlaceOrderListener listener;
+
+    // TODO 12. Set the listener once the fragment has been attached to the activity
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnPlaceOrderListener) {
+            listener = (OnPlaceOrderListener)context;
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.pizza_order, container, false);
+        MaterialButton placeOrderBtn = view.findViewById(R.id.place_order_button);
+        // TODO 13. We need to handle button click events to update the badge
+        placeOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onPlaceOrderButtonClicked();
+            }
+        });
         return view;
     }
 }
